f753568de44ae0362bbe010f53f516b7879c10c9
JuliIglesias <jiglesias@mail.austral.edu.ar>
2025-03-19 20:03:22 -0300

    fixedddddd nAVBAR ISSUE

--- app/src/main/java/com/example/quickStock/MainActivity.kt
+++ app/src/main/java/com/example/quickStock/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.example.quickStock.Home.ProductCategoryGrid
@@ -40,7 +41,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-
-
-

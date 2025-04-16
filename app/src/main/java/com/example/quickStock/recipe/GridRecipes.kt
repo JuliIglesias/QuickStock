d6fdfcf1500ef655c63ed7d4fef9e6491ef93ce6
JuliIglesias <jiglesias@mail.austral.edu.ar>
2025-04-16 19:00:57 -0300

    strings recipes in xml

--- app/src/main/java/com/example/quickStock/recipe/GridRecipes.kt
+++ app/src/main/java/com/example/quickStock/recipe/GridRecipes.kt
@@ -1,17 +1,10 @@
 package com.example.quickStock.recipe
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Cake
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.LocalDining
-import androidx.compose.material.icons.filled.LocalDrink
-import androidx.compose.material.icons.filled.LocalFlorist
-import androidx.compose.material.icons.filled.LocalGroceryStore
-import androidx.compose.material.icons.filled.SetMeal
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import com.example.quickStock.R
 import com.example.quickStock.common.CustomGrid
 import com.example.quickStock.icon.IconType
@@ -24,18 +17,18 @@ fun GridRecipes(
     val context = LocalContext.current
     val typeOfRecipes = context.resources.getStringArray(R.array.recipe_types)
     val typeOfRecipesIcons = mapOf(
-        "Fruits" to IconType.Drawable(painterResource(R.drawable.ic_fruit)),
-        "Vegetables" to IconType.Drawable(painterResource(R.drawable.ic_veggie)),
-        "Meat" to IconType.Drawable(painterResource(R.drawable.ic_meat)),
-        "Seafood" to IconType.Drawable(painterResource(R.drawable.ic_fish_cat)),
-        "Chicken" to IconType.Drawable(painterResource(R.drawable.ic_chicken)),
-        "Salads" to IconType.Drawable(painterResource(R.drawable.ic_salad)),
-        "Desserts" to IconType.Drawable(painterResource(R.drawable.ic_cookie)),
-        "Alcoholic Beverages" to IconType.Drawable(painterResource(R.drawable.ic_cocktail)),
-        "Pasta" to IconType.Drawable(painterResource(R.drawable.ic_pasta)),
-        "Bakery" to IconType.Drawable(painterResource(R.drawable.ic_cake)),
-        "Breakfast" to IconType.Drawable(painterResource(R.drawable.ic_egg)),
-        "Fast Food" to IconType.Drawable(painterResource(R.drawable.ic_hamburger)),
+        stringResource(R.string.fruits) to IconType.Drawable(painterResource(R.drawable.ic_fruit)),
+        stringResource(R.string.vegetables) to IconType.Drawable(painterResource(R.drawable.ic_veggie)),
+        stringResource(R.string.meat) to IconType.Drawable(painterResource(R.drawable.ic_meat)),
+        stringResource(R.string.seafood) to IconType.Drawable(painterResource(R.drawable.ic_fish_cat)),
+        stringResource(R.string.chicken) to IconType.Drawable(painterResource(R.drawable.ic_chicken)),
+        stringResource(R.string.salads) to IconType.Drawable(painterResource(R.drawable.ic_salad)),
+        stringResource(R.string.desserts) to IconType.Drawable(painterResource(R.drawable.ic_cookie)),
+        stringResource(R.string.alcoholic_beverages) to IconType.Drawable(painterResource(R.drawable.ic_cocktail)),
+        stringResource(R.string.pasta) to IconType.Drawable(painterResource(R.drawable.ic_pasta)),
+        stringResource(R.string.bakery) to IconType.Drawable(painterResource(R.drawable.ic_cake)),
+        stringResource(R.string.breakfast) to IconType.Drawable(painterResource(R.drawable.ic_egg)),
+        stringResource(R.string.fast_food) to IconType.Drawable(painterResource(R.drawable.ic_hamburger)),
     )
 
     val buttonDataList = typeOfRecipes.map { recipeString ->

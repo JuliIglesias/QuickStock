55d71b696450b02812e70282a5cc7d0751121e22
JuliIglesias <jiglesias@mail.austral.edu.ar>
2025-04-18 18:24:44 -0300

    RecipeData

---
+++ app/src/main/java/com/example/quickStock/recipeScreens/recipe/RecipeData.kt
@@ -0,0 +1,9 @@
+package com.example.quickStock.recipeScreens.recipe
+
+data class RecipeData(
+    val name: String,
+    val ingredients: List<String>,
+    val preparationTime: String,
+    val cookingTime: String? = null,
+    val steps: List<String>
+)

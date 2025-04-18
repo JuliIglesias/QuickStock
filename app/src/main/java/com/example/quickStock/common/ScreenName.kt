de13fff180176bc54a17883c200ff8b6b96472e0
JuliIglesias <jiglesias@mail.austral.edu.ar>
2025-04-18 18:00:12 -0300

    go back button in title

---
+++ app/src/main/java/com/example/quickStock/common/ScreenName.kt
@@ -0,0 +1,34 @@
+package com.example.quickStock.common
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ScreenName(onGoBack: () -> Unit, name:String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        GoBackButton {
+            onGoBack()
+        }
+
+        Text(
+            text = "Stock for $name",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.onSecondary
+        )
+    }
+}

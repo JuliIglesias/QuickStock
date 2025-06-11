ea3e87734ab821b58a10b92a9abb80466ff7879b
JuliIglesias <jiglesias@mail.austral.edu.ar>
2025-06-11 20:05:55 -0300

    storage local y propia en proceso

---
+++ app/src/main/java/com/example/quickStock/storage/LocalDataStorage.kt
@@ -0,0 +1,32 @@
+package com.example.quickStock.storage
+
+import android.content.Context
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+val Context.dataStore by preferencesDataStore(name = "QUICK_STOCK_DATA_STORE")
+
+object PreferencesKeys {
+    val USER_NAME_KEY = stringPreferencesKey("user_name_key")
+    val DARK_MODE_KEY = booleanPreferencesKey("dark_mode_key")
+    val NOTIFICATIONS_KEY = booleanPreferencesKey("notifications_key")
+}
+
+suspend fun <T> saveToDataStore(context: Context, value: T, key: Preferences.Key<T>) {
+    context.dataStore.edit { preferences ->
+        preferences[key] = value
+    }
+}
+
+fun <T> getFromDataStore(context: Context, key: Preferences.Key<T>): Flow<T?> {
+    return context.dataStore.data
+        .map { preferences ->
+            preferences[key]
+        }
+}
+
---
+++ app/src/main/java/com/example/quickStock/storage/RemoteDataStorage.kt
@@ -0,0 +1,30 @@
+package com.example.quickStock.storage
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.quickStock.data.ProductDao
+
+
+@Database(entities = [Product::class], version = 1)
+abstract class QuickStockDatabase : RoomDatabase() {
+    abstract fun productDao(): ProductDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: QuickStockDatabase? = null
+
+        fun getDatabase(context: Context): QuickStockDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    QuickStockDatabase::class.java,
+                    "quick_stock_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
